5592141e41769b6206853cafce0d12bc2302b145
gespinn <gerardoespinoza940@yahoo.com>
2025-03-27 15:00:07 -0700

    Checks Element type
    Displays element type in console

--- Element_Highlighter/content.js
+++ Element_Highlighter/content.js
@@ -1,6 +1,26 @@
 document.addEventListener("mouseover", function (event) {
     let target = event.target;
 
+    let elementType = "Unknown";
+    if (target.tagName === "IMG") {
+        elementType = "Image";
+    } 
+    else if (target.tagName === "P" || target.tagName === "DIV") {
+        elementType = "Text";
+    } 
+    else if (target.tagName === "A") {
+        elementType = "Link";
+    } 
+    else if (target.tagName === "BUTTON") {
+        elementType = "Button";
+    } 
+    else if (target.tagName === "INPUT" || target.tagName === "TEXTAREA") {
+        elementType = "Input Field";
+    }
+
+    //Logs element type in console
+    console.log(`Hovered element type: ${elementType}`);
+
     // Add highlight class
     target.classList.add("hover-highlight");
 
