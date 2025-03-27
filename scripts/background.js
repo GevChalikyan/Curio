8b65bd6ac68a94a0285037f599b49c66dfe3c96a
Gev Chalikyan <72428068+GevChalikyan@users.noreply.github.com>
2025-03-27 14:35:07 -0700

    Changed Macro Name and Added Conditional Statement for When Triggered

--- scripts/background.js
+++ scripts/background.js
@@ -1,3 +1,7 @@
 chrome.commands.onCommand.addListener((command) => {
   console.log(`Command "${command}" triggered`);
+
+  if (command === "Element_Selection_State") {
+    console.log("Element Selection State Enabled");
+  }
 });
