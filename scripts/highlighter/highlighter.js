4f0dcd49a9fdf9e3392373e772a1d2f08c85e1ae
Gev Chalikyan <72428068+GevChalikyan@users.noreply.github.com>
2025-03-27 20:34:33 -0700

    Added Toggle For Element Highlighting
    Not functional yet, needs debugging

--- scripts/highlighter/highlighter.js
+++ scripts/highlighter/highlighter.js
@@ -1,4 +1,6 @@
-document.addEventListener("mouseover", function (event) {
+is_element_selection_active = false;
+
+function element_selection_listener(event) {
   let target = event.target;
 
   target.classList.add("hover-highlight");
@@ -6,4 +8,22 @@ document.addEventListener("mouseover", function (event) {
   target.addEventListener("mouseout", function () {
       target.classList.remove("hover-highlight");
   }, { once: true });
-});
+}
+
+function toggle_element_selection() {
+  if (isHighlighterActive) {
+    document.addEventListener("mouseover", element_selection_listener);
+    console.log("Element Selection Enabled");
+  } else {
+    document.removeEventListener("mouseover", element_selection_listener);
+    console.log("Element Selection Disabled");
+  }
+  isHighlighterActive = !isHighlighterActive;
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "toggle_element_selection") {
+    toggle_element_selection();
+    sendResponse({ status: is_element_selection_active ? "Enabled" : "Disabled" });
+  }
+})
