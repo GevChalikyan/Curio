f2138c5fc5a8eaa00bb099323729934a0c067200
Gev Chalikyan <72428068+GevChalikyan@users.noreply.github.com>
2025-04-29 19:55:51 -0700

    Changes to Logging

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // --- Middleware ---
 
@@ -30,7 +30,6 @@ app.post('/echo', (req, res) => {
   });
 });
 
-// --- Start Server ---
 app.listen(PORT, () => {
-  console.log(`🚀 Server listening on http://localhost:${PORT}`);
+  console.log(`🚀 Server listening on port ${PORT}`);
 });
