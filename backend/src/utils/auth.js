e8e80d7c1ed077869f748f840e95f0e4838194c1
Gev Chalikyan <72428068+GevChalikyan@users.noreply.github.com>
2025-05-01 18:29:21 -0700

    Bug Fix
    Added required response to the login endpoint
    Added router to exports

--- backend/src/utils/auth.js
+++ backend/src/utils/auth.js
@@ -1,11 +1,15 @@
 const { query } = require("../db/db");
 const bcrypt = require("bcrypt");
 
-const router = require('express').Router;
+const router = require('express').Router();
 
 // Login Endpoint
 router.post('/login', (req, res) => {
   
   console.log('Login Accessed');
   
+  res.status(200).json({ message: 'ok' });
+
 });
+
+module.exports = router;
