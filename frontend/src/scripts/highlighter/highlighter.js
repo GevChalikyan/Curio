21e4ac33467639aaad6f12e48e3052c5f38361ca
Gev Chalikyan <72428068+GevChalikyan@users.noreply.github.com>
2025-05-11 13:46:42 -0700

    Fixed a Small Formatting Inconsistency

--- frontend/src/scripts/highlighter/highlighter.js
+++ frontend/src/scripts/highlighter/highlighter.js
@@ -1,22 +1,22 @@
 is_element_selection_active = false;
 
-  function rgbToHsl(r, g, b) {
-    r /= 255; g /= 255; b /= 255;
-    const max = Math.max(r, g, b), min = Math.min(r, g, b);
-    let h, s, l = (max + min) / 2;
-    if (max === min) {
-        h = s = 0;
-    } else {
-        const d = max - min;
-        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-        switch (max) {
-            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-            case g: h = (b - r) / d + 2; break;
-            case b: h = (r - g) / d + 4; break;
-        }
-        h *= 60;
-    }
-    return [h, s * 100, l * 100];
+function rgbToHsl(r, g, b) {
+  r /= 255; g /= 255; b /= 255;
+  const max = Math.max(r, g, b), min = Math.min(r, g, b);
+  let h, s, l = (max + min) / 2;
+  if (max === min) {
+      h = s = 0;
+  } else {
+      const d = max - min;
+      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+      switch (max) {
+          case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+          case g: h = (b - r) / d + 2; break;
+          case b: h = (r - g) / d + 4; break;
+      }
+      h *= 60;
+  }
+  return [h, s * 100, l * 100];
 }
 
 function element_selection_listener(event) {
